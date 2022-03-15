8137b857023fb1e5a52bf3b553a28f18e03efd22
aidevnn <ilyas.kadassi@gmail.com>
2022-03-15 19:55:04 +0300

    First Commit

--- FiniteGroup/Program.cs
+++ FiniteGroup/Program.cs
@@ -6,21 +6,19 @@ namespace FiniteGroup
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             var s4 = new Sigma(4);
-            s4.DisplayGroup(s4.PCycle(3));
-            s4.DisplayGroup(s4.Tau(2), s4.Tau(3));
+            //s4.DisplayGroup(s4.PCycle(3));
+            //s4.DisplayGroup(s4.Tau(2), s4.Tau(3));
 
-            s4.TableGroup(s4.Tau(2));
-            s4.TableGroup(s4.PCycle(3));
-            s4.TableGroup(s4.Tau(2), s4.Tau(3, 4));
-            s4.TableGroup(s4.Tau(2), s4.Tau(3));
+            //s4.TableGroup(s4.Tau(2));
+            //s4.TableGroup(s4.PCycle(3));
+            //s4.TableGroup(s4.Tau(2), s4.Tau(3, 4));
+            //s4.TableGroup(s4.Tau(2), s4.Tau(3));
 
-            s4.DetailGroup(s4.Tau(2));
-            s4.DetailGroup(s4.PCycle(3));
+            //s4.DetailGroup(s4.Tau(2));
+            //s4.DetailGroup(s4.PCycle(3));
             s4.DetailGroup(s4.Tau(2), s4.Tau(3, 4));
-            s4.DetailGroup(s4.Tau(2), s4.Tau(3));
+            //s4.DetailGroup(s4.Tau(2), s4.Tau(3));
 
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # FiniteGroup
 Group theory
+
+```
+var s4 = new Sigma(4);
+s4.DetailGroup(s4.Tau(2), s4.Tau(3, 4));
+```
+
+Will output the permutations and the generated  group table
+
+```
+[ 1  2  3  4](@)
+[ 1  2  3  4](1)
+
+[ 1  2  3  4](a)
+[ 1  2  4  3](2)
+
+[ 1  2  3  4](b)
+[ 2  1  3  4](2)
+
+[ 1  2  3  4](c)
+[ 2  1  4  3](2)
+
+ *|@ a b c
+--|--------
+ @|@ a b c
+ a|a @ c b
+ b|b c @ a
+ c|c b a @
+```
